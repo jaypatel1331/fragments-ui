45d0f236a8bee8ff0d2a252328ce811c3c2465f3
Jay Patel <jaykumarpatel2410@gmail.com>
2023-01-19 17:19:27 -0500

    final changes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 // src/app.js
 
 import { Auth, getUser } from './auth';
+import { getUserFragments } from './api';
+
 
 async function init() {
   // Get our UI elements
@@ -22,6 +24,10 @@ async function init() {
 
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
+
+  // Do an authenticated request to the fragments API server and log the result
+  getUserFragments(user);
+  
   if (!user) {
     // Disable the Logout button
     logoutBtn.disabled = true;
