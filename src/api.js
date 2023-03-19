c9cdcd4348ed11b7403524fe9ec3d234e832027d
Jay Patel <jaykumarpatel2410@gmail.com>
2023-03-19 15:00:32 -0400

    changes for apiurl

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 // src/api.js
 
 // fragments microservice API, defaults to localhost:8080
-//const apiUrl = process.env.API_URL || 'http://localhost:8080/';
-const apiUrl = 'http://localhost:8080/';
+const apiUrl = process.env.API_URL || 'http://localhost:8080/';
+
 /**
  * Given an authenticated user, request all fragments for this user from the
  * fragments microservice (currently only running locally). We expect a user
